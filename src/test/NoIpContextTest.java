1c88692901b6a5bb25aefcc20d3fedb7f25ae715
rmorawski <radoslaw.morawski@opi.org.pl>
2016-08-25 16:03:45 +0200

    Prace nad wersją 1.0 - testy

--- src/test/NoIpContextTest.java
+++ src/test/NoIpContextTest.java
@@ -1,8 +1,3 @@
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -10,9 +5,7 @@ import pl.radmit.log.Logger;
 import pl.radmit.noIpService.models.NoIpAccount;
 import pl.radmit.noIpService.services.NoIpApiService;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 public class NoIpContextTest {
 
@@ -62,52 +55,4 @@ public class NoIpContextTest {
 
         apiService.writeNewIpToCfgFile(actualIp);
     }
-
-    @Test
-    public void sendNewIpToNoIpServer() throws IOException {
-        NoIpAccount noIpAccount = Mockito.mock(NoIpAccount.class);
-        Logger logger = Mockito.mock(Logger.class);
-
-        HttpClient httpClient = Mockito.mock(HttpClient.class);
-        HttpResponse httpResponse = Mockito.mock(HttpResponse.class);
-
-        StatusLine statusLine = Mockito.mock(StatusLine.class);
-        Mockito.when(statusLine.getStatusCode()).thenReturn(200);
-
-        HttpEntity httpEntity = Mockito.mock(HttpEntity.class);
-        Mockito.when(httpEntity.getContent()).thenReturn(new ByteArrayInputStream("good 1.1.1.1".getBytes(StandardCharsets.UTF_8)));
-
-
-        Mockito.when(httpResponse.getStatusLine()).thenReturn(statusLine);
-        Mockito.when(httpResponse.getEntity()).thenReturn(httpEntity);
-        Mockito.when(httpClient.execute(Mockito.any())).thenReturn(httpResponse);
-
-
-        NoIpApiService apiService = new NoIpApiService(noIpAccount, logger);
-        apiService.sendNewIpToNoIpServer("8.8.8.8", "1.1.1.1", httpClient);
-    }
-
-    @Test(expected = ClientProtocolException.class)
-    public void badResponseCodeFromNoip() throws IOException {
-        NoIpAccount noIpAccount = Mockito.mock(NoIpAccount.class);
-        Logger logger = Mockito.mock(Logger.class);
-
-        HttpClient httpClient = Mockito.mock(HttpClient.class);
-        HttpResponse httpResponse = Mockito.mock(HttpResponse.class);
-
-        StatusLine statusLine = Mockito.mock(StatusLine.class);
-        Mockito.when(statusLine.getStatusCode()).thenReturn(404);
-
-        HttpEntity httpEntity = Mockito.mock(HttpEntity.class);
-        Mockito.when(httpEntity.getContent()).thenReturn(new ByteArrayInputStream("good 1.1.1.1".getBytes(StandardCharsets.UTF_8)));
-
-
-        Mockito.when(httpResponse.getStatusLine()).thenReturn(statusLine);
-        Mockito.when(httpResponse.getEntity()).thenReturn(httpEntity);
-        Mockito.when(httpClient.execute(Mockito.any())).thenReturn(httpResponse);
-
-        NoIpApiService apiService = new NoIpApiService(noIpAccount, logger);
-        apiService.sendNewIpToNoIpServer("8.8.8.8", "1.1.1.1", httpClient);
-    }
-
 }
